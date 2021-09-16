6c9fbe221e8a431c6b435bf616eae4d4cbba4c2a
Trinity Christiana <trinitycterry@gmail.com>
2021-09-15 23:27:09 -0500

    fixed button spaces

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,39 +1,39 @@
 import { DOWN, LEFT, RIGHT, UP, START } from '../../constants';
 import { Button } from 'reactstrap';
 import { ArrowDown, ArrowLeft, ArrowRight, ArrowUp, RefreshCcw } from 'react-feather';
-
+import './buttons.scss';
 const Buttons = ({ handleAction }) => {
   return (
     <div className='d-flex'>
       <div className='d-flex m-3 flex-column'>
         <div className='d-flex'>
-          <div style={{ height: 50, width: 50, clear: 'both' }}> </div>
-          <Button style={{ height: 50, width: 50 }} onClick={() => handleAction(UP)}>
+          <div className='direction-button-spacer'></div>
+          <Button className='app-button direction-button' onClick={() => handleAction(UP)}>
             <ArrowUp />
           </Button>
-          <div style={{ height: 50, width: 50, clear: 'both' }}> </div>
+          <div className='direction-button-spacer'></div>
         </div>
         <div className='d-flex'>
-          <Button style={{ height: 50, width: 50 }} onClick={() => handleAction(LEFT)}>
+          <Button className='app-button direction-button' onClick={() => handleAction(LEFT)}>
             <ArrowLeft />
           </Button>
 
-          <div style={{ height: 50, width: 50, clear: 'both' }}></div>
-          <Button style={{ height: 50, width: 50 }} onClick={() => handleAction(RIGHT)}>
+          <div className='direction-button-spacer'></div>
+          <Button className='app-button direction-button' onClick={() => handleAction(RIGHT)}>
             <ArrowRight />
           </Button>
         </div>
         <div className='d-flex'>
           {' '}
-          <div style={{ height: 50, width: 50, clear: 'both' }}> </div>
-          <Button style={{ height: 50, width: 50 }} onClick={() => handleAction(DOWN)}>
+          <div className='direction-button-spacer'></div>
+          <Button className='app-button direction-button' onClick={() => handleAction(DOWN)}>
             <ArrowDown />
           </Button>{' '}
-          <div style={{ height: 50, width: 50, clear: 'both' }}> </div>
+          <div className='direction-button-spacer'></div>
         </div>
       </div>
       <div className='m-3 align-self-center'>
-        <Button onClick={() => handleAction(START)}>
+        <Button className='app-button' onClick={() => handleAction(START)}>
           Restart <RefreshCcw />
         </Button>
       </div>
