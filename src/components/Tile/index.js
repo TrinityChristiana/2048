b5ca2b4333c87619752e6ecf7a541720dd3a2ece
Trinity Christiana <trinitycterry@gmail.com>
2021-09-15 19:33:44 -0500

    updated sass

--- src/components/Tile/index.js
+++ src/components/Tile/index.js
@@ -1,7 +1,7 @@
 import './tile.scss';
 const Tile = ({ value }) => {
   return (
-    <span className={`tile-num-${value === 0 ? 'empty' : value} text-center`} style={{ width: 100, height: 100, fontSize: 48, border: '8px solid #bbada0' }}>
+    <span className={`tile-num-${value} text-center`} style={{ width: 100, height: 100, fontSize: 48, border: '8px solid #bbada0' }}>
       {value}
     </span>
   );
