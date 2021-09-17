a907b30b4c752452c1f258203be9096f09d22f30
Trinity Christiana <trinitycterry@gmail.com>
2021-09-16 22:35:57 -0500

    Added touch stuff...

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import Game from './components/Game';
 import './App.scss';
+import { useEffect } from 'react';
 function App() {
+  useEffect(() => {
+    document.addEventListener('touchmove', (e) => {
+      e.preventDefault();
+      e.stopPropagation()
+    });
+    // $(function () {
+    //   $(document).on('touchmove', function (evt) {
+    //     evt.preventDefault();
+    //   });
+    //   $(document).on('touchmove', '.scrollable', function (evt) {
+    //     evt.stopPropagation();
+    //   });
+    // });
+  }, []);
   return <Game />;
 }
 
