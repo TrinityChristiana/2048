fa285df6237e9c94fa4e36aa2b8f85badd91cf70
Trinity Christiana <trinitycterry@gmail.com>
2021-09-18 21:41:42 -0500

    formatted stuff

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import Game from './components/Game';
-import './App.scss';
 import { useEffect } from 'react';
+import './App.scss';
+import Game from './components/Game';
 function App() {
   useEffect(() => {
     document.addEventListener('touchmove', (e) => {
       e.preventDefault();
       e.stopPropagation();
     });
-
   }, []);
   return <Game />;
 }
