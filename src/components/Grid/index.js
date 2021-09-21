d42dfd82d7f77bde63c38009975ced7c0f94ac0a
Trinity Christiana <trinitycterry@gmail.com>
2021-09-20 20:28:16 -0500

    added scalability to grid

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -5,8 +5,8 @@ const Grid = ({ tileData, gameData, screenWidth }) => {
   const [gridHTML, setGridHTML] = useState(<div>Loading...</div>);
 
   useEffect(() => {
-    const normalHeight = 816;
-    const [tileSize, gridSize] = screenWidth >= normalHeight ? [(normalHeight - 16) / 4, normalHeight] : [(screenWidth - 16) / 4, screenWidth];
+    const normalWidth = 416;
+    const [tileSize, gridSize, fontSize] = screenWidth >= normalWidth ? [(normalWidth - 16) / 4, normalWidth, 100] : [(screenWidth - 16) / 4, screenWidth, Math.floor((screenWidth / normalWidth) * 100)];
     let tempHTML = (
       <div
         style={{
@@ -20,7 +20,7 @@ const Grid = ({ tileData, gameData, screenWidth }) => {
           <div key={`grid-${x}`} className='d-flex'>
             {Array.from(new Array(gameData.yValue)).map((_, y) => {
               const tile = tileData.find((element) => element.x === x && element.y === y);
-              return <Tile key={`${x}-${y}`} size={tileSize} value={tile ? tile.value : 0} />;
+              return <Tile key={`${x}-${y}`} size={tileSize} fontSize={fontSize} value={tile ? tile.value : 0} />;
             })}
           </div>
         ))}
--- src/components/Tile/index.js
+++ src/components/Tile/index.js
@@ -1,7 +1,7 @@
 import './tile.scss';
-const Tile = ({ value, size }) => {
+const Tile = ({ value, size, fontSize }) => {
   return (
-    <span className={`tile tile-num-${value > 2048 ? 'super' : value}`} style={{ height: size, width: size }}>
+    <span className={`tile tile-num-${value > 2048 ? 'super' : value} tile-num-size--${fontSize}`} style={{ height: size, width: size }}>
       {value}
     </span>
   );
